f5db7cb2feb250473a2c45ae3f16d48c257a5292
Michael Larocque <michaellarocque@MacBook-Pro.local>
2021-07-12 03:00:14 -0400

    New Post page styling.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import sanityClient from "../client";
 
 export default function Post() {
@@ -17,7 +18,29 @@ export default function Post() {
                     alt
                 }
             }`)
-    })
+            .then((data) => setPost(data))
+            .catch(console.error);
+    }, []);
     
-    return 
+    return (
+        <main className="bg-green-100 min-h-screen p-12">
+            <section className="container mx-auto">
+                <h1 className="text-5xl flex justify-center sans-serif">Blog Post Page</h1>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    <article>
+                        <Link>
+                         <span>
+                            <img />
+                            <span>
+                                <h3>
+                                </h3>
+                            </span>
+                         </span>
+                        </Link>
+                    </article>
+                </div>
+            </section>
+        </main>
+    )
 }
